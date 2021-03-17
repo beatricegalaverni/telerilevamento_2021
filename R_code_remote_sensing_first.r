ec33b8e84bbcc5ff8cbff5815e6b1daed61004a4
beatricegalaverni <80753697+beatricegalaverni@users.noreply.github.com>
2021-03-17 12:57:15 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -4,3 +4,9 @@ library(raster)
 p224r63_2011<-brick("p224r63_2011_masked.grd")
 p224r63_2011
 plot(p224r63_2011)
+# Per cambiare la scala dei colori con i quali visualizziamo la nostra immagine
+colore<-colorRampPalette(c("black","gray","light gray"))(100)
+plot(p224r63_2011, col=colore)
+# Cambiare colore ed avere scala di colori differenti 
+colore<-colorRampPalette(c("pink","black","green"))(100)
+plot(p224r63_2011, col=colore)
