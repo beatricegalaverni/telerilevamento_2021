6072d532840376011f88fdf12450ba16733b89d2
beatricegalaverni <80753697+beatricegalaverni@users.noreply.github.com>
2022-07-11 18:20:09 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -2,5 +2,7 @@
 # utilizziamo il pacchetto knitr per creare un report in R
 library(knitr)
 setwd("C:/lab/")
+# utilizziamo la funzione stitch, il primo argomento è come si chiama il nostro codice, poi il tamplate e il pacchetto da utilizzare 
+stitch("R_code_greenlad.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
 
