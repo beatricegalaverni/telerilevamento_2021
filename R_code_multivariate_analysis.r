f0256d95420bc478f4526e511533fda162dee14a
beatricegalaverni <80753697+beatricegalaverni@users.noreply.github.com>
2022-07-12 17:47:40 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -16,17 +16,23 @@ plot(p224r63_2011$B1_sre, p224r63_2011$B2_sre, col="red", pch=19, cex=2)
 plot(p224r63_2011$B2_sre, p224r63_2011$B1_sre, col="red", pch=19, cex=2)
 # attraverso la funzione pairs, plottiamo tutte le correlazioni possibili tra la bande di un dataset
 pairs(p224r63_2011)
-# aggregate cells: resampling (ricampionamento)
+# usiamo la funzione aggregate per aggregare i pixel e avere una risoluzione più bassa per avere un dato un po' più leggero
+# attraverso factor decidiamo di quante volte aumentare la grandezza del pixel
+# questo processo si chiama ricampionamento, da qui il nome "res"
 p224r63_2011res <- aggregate(p224r63_2011, fact=10)
-
+# facciamo un pannello con due immagini dentro per confrontare le due immagini 
 par(mfrow=c(2,1))
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
 plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin")
-
+# Analisi delle componenti principali (PCA)
+# attraverso la funzione "rasterPCA" prendiamo il pacchetto di dati e li compatti lungo un numero minore di bande  
 p224r63_2011res_pca <- rasterPCA(p224r63_2011res)
-
+# utilizziamo la funzione summary che ci da un sommario del modello 
 summary(p224r63_2011res_pca$model)
-
-# dev.off()
+# andiamo a fare un plot totale, vedremo tutte le componenti, la prima con il massimo della varianza spiegata 
+plotRGB(p224r63_2011res_pca$map)
+# plottiamo in RGB l'immagine legata al modello usando uno stretch lineare 
 plotRGB(p224r63_2011res_pca$map, r=1, g=2, b=3, stretch="lin")
+
+
 
