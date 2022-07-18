f90033ce1f5771b852364e375c51735913e2df8c
beatricegalaverni <80753697+beatricegalaverni@users.noreply.github.com>
2022-07-18 17:53:00 +0200

    Update R_code_spectral_signatures.r

--- R_code_spectral_signatures.r
+++ R_code_spectral_signatures.r
@@ -17,26 +17,22 @@ plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
 #pch (per definir eil tipo di pinuto) cex (per la dimensione), col (per il colore)
 click(defor2, id=T, xy=T, cell=T, type="p", pch=16, col="magenta") 
 # risultati
-
-
-# costruiamo ua colonna con i valori di riflettanza per la banda 1, 2 e 3 per la foresta e per l'acqua
+#x     y   cell defor2.1 defor2.2 defor2.3
+#1 155.5 270.5 148575      210       18       29
+#      x     y   cell defor2.1 defor2.2 defor2.3
+#1 120.5 219.5 185107      139       29       56
+# costruiamo una colonna con i valori di riflettanza per la banda 1, 2 e 3 per la foresta e per l'acqua
 band <- c(1,2,3)
-forest <- c(187,23,34)
-water <- c(39,87,125)
-
-# col.lab="red")
-# press esc or close window
-
-# store data
-dw <- c(1,2,3)
-forest <- c(187,23,34)
-water <- c(39,87,125)
-
-output <- data.frame(dw,forest,water)
-attach(output)
- 
-library(ggplot2)
-ggplot(output, aes(x=dw)) + 
+forest <- c(210,18,29)
+water <- c(139,29,56)
+# attraverso la funzione data.frame costruiamo una tabella 
+spectrals <- data.frame(band,forest,water)
+attach(spectrals)
+# attraverso la funzione ggplot andiamo a plottare 
+# attraverso le aesteticts definiamo cosa deve essere l'asse delle x, mettiamo le bande
+# sull'asse delle y mettiamo la riflettanza mettendo una linea per la foresta e una per l'acqua 
+# attrvaerso le funzione geom_line inseriamo le geometria nel plot 
+ggplot(output, aes(x=band)) + 
     geom_line(aes(y = forest), color = "green")+
     geom_line(aes(y = water), color = "blue", linetype = "dotted")+
     labs(x="wavelength", y="reflectance")
